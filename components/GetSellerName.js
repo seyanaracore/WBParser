109143b2655b5b2aa87d6ab83c6f6ff6330e74fd
Anton Tigunov <seyanaracore@gmail.com>
2022-07-29 14:12:49 +0300

    ext settings file

--- components/GetSellerName.js
+++ components/GetSellerName.js
@@ -1,4 +1,4 @@
-import settings from "../utils/settings";
+import settings from "../utils/settings.js";
 
 export default () => settings.url.split("seller/")[1]?.split("/")[0]?.split("?")[0] ||
    settings.url.split("brands/")[1]?.split("/")[0]?.split("?")[0] ||
